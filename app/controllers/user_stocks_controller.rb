ea6dfe0dfab78173adfed6c949cbc217812d23da
Amritansh Jain <amitansh.jain@gmail.com>
2021-07-21 18:06:46 +0530

    Added many-to-many association between users and stocks

---
+++ app/controllers/user_stocks_controller.rb
@@ -0,0 +1,13 @@
+class UserStocksController < ApplicationController
+
+  def create
+    stock = Stock.check_stock_in_db(params[:ticker])
+    if stock.blank?
+      stock = Stock.new_lookup(params[:ticker])
+      stock.save
+    end
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "Stock #{stock.name} was successfully added to your Portfolio"
+    redirect_to my_portfolio_path
+  end
+end
