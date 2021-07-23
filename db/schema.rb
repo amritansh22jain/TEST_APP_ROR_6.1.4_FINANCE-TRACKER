83317efac036fdd050ac205178b53700cab12808
Amritansh Jain <amitansh.jain@gmail.com>
2021-07-23 20:02:03 +0530

    Corrected the Last Name datatype in Users table in migration file

--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,19 @@ ActiveRecord::Schema.define(version: 2021_07_21_152156) do
     t.index ["user_id"], name: "index_user_stocks_on_user_id"
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'sting' for column 'last_name'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
   add_foreign_key "friendships", "users"
   add_foreign_key "friendships", "users", column: "friend_id"
